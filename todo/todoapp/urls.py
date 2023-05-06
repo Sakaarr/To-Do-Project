0d86a8979789cb463ebb0fb1bf3778936db424f4
Sakar Bhandari <sakarbhandari100000@gmail.com>
2023-05-06 22:59:21 +0545

    Project Completerd

--- todo/todoapp/urls.py
+++ todo/todoapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path 
 from . import views
 urlpatterns = [
-    path('' , views.addanddisplay , name='addanddisplay')
+    path('' , views.addanddisplay , name='addanddisplay'),
+    path('delete/<int:id>' , views.delete_data , name='delete'),
+    path('<int:id>/' , views.update_data , name='update'),
 ]
--- todo/todoapp/views.py
+++ todo/todoapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .forms import Form
+from .models import User
+from django.http import HttpResponseRedirect
 # Create your views here.
 def addanddisplay(request):
     if request.method=="POST":
@@ -8,5 +10,22 @@ def addanddisplay(request):
             fm.save()
             fm=Form()
     else:
-        fm=Form()        
-    return render(request , 'todoapp/addanddisplay.html' , {'forms':fm})
+        fm=Form()   
+    info = User.objects.all()     
+    return render(request , 'todoapp/addanddisplay.html' , {'forms':fm,'info':info})
+def delete_data(request , id):
+    if request.method=="POST":
+        pi = User.objects.get(pk=id)
+        pi.delete()
+        return HttpResponseRedirect('/')
+def update_data(request , id):
+    if request.method=="POST":
+        pi = User.objects.get(pk=id)
+        fm = Form(request.POST , instance= pi)
+        if fm.is_valid:
+            fm.save()
+    else:
+        if request.method=="GET":
+            pi = User.objects.get(pk=id)
+            fm = Form(instance=pi)
+    return render(request , 'todoapp/update.html', {'forma':fm})
