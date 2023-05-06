b50fa84a3fb2b2453122d257a6e3c23d89ff34fe
Sakar Bhandari <sakarbhandari100000@gmail.com>
2023-05-06 14:54:29 +0545

    Input Form Created

--- todo/todoapp/models.py
+++ todo/todoapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    task_name = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now=True)
+    
--- todo/todoapp/views.py
+++ todo/todoapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .forms import Form
 # Create your views here.
+def addanddisplay(request):
+    if request.method=="POST":
+        fm=Form(request.POST)
+        if fm.is_valid():
+            fm.save()
+            fm=Form()
+    else:
+        fm=Form()        
+    return render(request , 'todoapp/addanddisplay.html' , {'forms':fm})
